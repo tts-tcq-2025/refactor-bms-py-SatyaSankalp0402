2bc8c23f5d45a5402b4959f6c2e2764152484667
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-07-29 10:52:28 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,7 +2,7 @@
 from time import sleep
 import sys
 
-def warning(cycles=6,sleep_time=1):
+def display_warning(cycles=6,sleep_time=1):
   for i in range(cycles):
       print('\r* ', end='')
       sys.stdout.flush()
@@ -11,20 +11,20 @@ def warning(cycles=6,sleep_time=1):
       sys.stdout.flush()
       sleep(sleep_time)
     
-def show_warning(message):
+def show_warning_message(message):
   print(message)
   
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
-    show_warning('Temperature critical!')
-    warning()
+    show_warning_message('Temperature critical!')
+    display_warning()
     return False
   elif pulseRate < 60 or pulseRate > 100:
-    show_warning('Pulse Rate is out of range!')
-    warning()
+    show_warning_message('Pulse Rate is out of range!')
+    display_warning()
     return False
   elif spo2 < 90:
-    show_warning('Oxygen Saturation out of range!')
-    warning()
+    show_warning_message('Oxygen Saturation out of range!')
+    display_warning()
     return False
   return True
