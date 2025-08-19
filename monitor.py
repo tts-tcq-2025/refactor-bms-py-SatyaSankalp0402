a1261b7632fc2dcf9e4f202fea3e25de3c698014
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-08-19 09:21:50 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,10 @@
 from warning import blinking_alert
 from warning import print_warning_message
-
- global value_vitals = [
+global value_vitals = [
         (temperature, 95, 102, 'Temperature critical!'),
         (pulseRate, 60, 100, 'Pulse Rate is out of range!'),
         (spo2, 90, float('inf'), 'Oxygen Saturation out of range!')
-    ]
+]
   
 def vitals_ok(temperature, pulseRate, spo2):
    
