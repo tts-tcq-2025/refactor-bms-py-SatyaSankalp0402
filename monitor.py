989f075e53ae3fd40b3faf8522d12118645300d3
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-08-19 09:31:02 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,12 +1,12 @@
 from warning import blinking_alert
 from warning import print_warning_message
-value_vitals = [
+
+def vitals_ok(temperature, pulseRate, spo2):
+    value_vitals = [
         (temperature, 95, 102, 'Temperature critical!'),
         (pulseRate, 60, 100, 'Pulse Rate is out of range!'),
         (spo2, 90, float('inf'), 'Oxygen Saturation out of range!')
-]
-  
-def vitals_ok(temperature, pulseRate, spo2):
+        ]    
     for value, low, high, message in value_vitals:
       if (value < low) or (value > high):
           print_warning_message(message)
