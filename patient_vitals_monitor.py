de134833cfd22b09ed8108c52f006f2bc8b68cc7
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-08-19 10:04:16 +0530

    Update patient_vitals_monitor.py

--- patient_vitals_monitor.py
+++ patient_vitals_monitor.py
@@ -1,20 +1,20 @@
 from warning import blinking_alert
 from warning import print_warning_message
 
-value_vitals = [
+vital_thresholds = [
     ("temperature", 95, 102, "Temperature critical!", "Approaching Hypothermia", "Approaching Hyperthermia"),
     ("pulseRate", 60, 100, "Pulse Rate is out of range!", "Pulse dropping low", "Pulse Rising High"),
     ("spo2", 90, float("inf"), "Oxygen Saturation out of range!","Oxygen level dropping", None)
 ]
 
-def vitals_ok(temperature, pulseRate, spo2):
-    vitals = {
+def check_vital_signs(temperature, pulseRate, spo2):
+    current_vitals = {
         "temperature": temperature,
         "pulseRate": pulseRate,
         "spo2": spo2
     }
-    for vital_name, low, high, alert_message, warn_low_message, warn_high_message in value_vitals:
-      value = vitals[vital_name]
+    for vital_name, low, high, alert_message, warn_low_message, warn_high_message in value_thresholds:
+      value = current_vitals[vital_name]
       if high== float("inf"):
           tolerance=0
       else:
