2e76f3ad71d656c1289fee053be2f49f9c50fa11
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-07 21:11:48 +0530

    Update patient_vitals_monitor.py

--- patient_vitals_monitor.py
+++ patient_vitals_monitor.py
@@ -6,6 +6,25 @@ vital_thresholds = {
     "pulseRate": (60, 100, "Pulse Rate is out of range!", "Pulse dropping low", "Pulse Rising High"),
     "spo2": (90, float("inf"), "Oxygen Saturation out of range!","Oxygen level dropping", None)
 }
+def calculate_tolerance(high):
+    if high==float("inf"):
+        return 0
+    else:
+        return high*0.015
+
+def check_warnings(value, low, high, tolerance, warn_low_message, warn_high_message):
+    if warn_low_message and low <= value < low + tolerance:
+        print_warning_message(f"Warning: {warn_low_message}")
+    if warn_high_message and high - tolerance < value <= high:
+        print_warning_message(f"Warning: {warn_high_message}")
+
+def check_critical(value, low, high, alert_message):
+    if value < low or value > high:
+        print_warning_message(alert_message)
+        blinking_alert()
+        return False
+    return True
+
 
 def check_vital_signs(temperature, pulseRate, spo2):
     current_vitals = {
@@ -15,20 +34,8 @@ def check_vital_signs(temperature, pulseRate, spo2):
     }
     for vital_name,value in current_vitals.items():
       low,high,alert_message,warn_low_message,warn_high_message = vital_thresholds[vital_name]
-      if high== float("inf"):
-          tolerance=0
-      else:
-          tolerance=high*0.015
-
-      #Early Warnings Detection
-      if warn_low_message and (low<= value < low+tolerance):
-          print_warning_message(f"Warning: {warn_low_message}")
-      if warn_high_message and (high-tolerance < value <=high):
-          print_warning_message(f"Warning: {warn_high_message}")
-
-      # Critical Alerts    
-      if (value < low) or (value > high):
-          print_warning_message(alert_message)
-          blinking_alert()
-          return False
+      tolerance=calculate_tolerance(high)
+      check_warnings(value, low, high, tolerance, warn_low, warn_high)
+      if not check_critical(value, low, high, alert_message):
+            return False
     return True
